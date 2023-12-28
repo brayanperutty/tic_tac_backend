e0455e9b97f89deb3775ccbeddc0916d83069a27
brayanperutty <brayanperutty@gmail.com>
2023-12-28 10:00:45 -0500

    Se ajusta la configuración de CORS

--- src/main/java/com/tictac/demo/config/CorsConfig.java
+++ src/main/java/com/tictac/demo/config/CorsConfig.java
@@ -13,8 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://tictacbackend-production-28ad.up.railway.app", "http://localhost:8080")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("https://tictacbackend-production-28ad.up.railway.app", "http://localhost:*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*");
             }
         };
