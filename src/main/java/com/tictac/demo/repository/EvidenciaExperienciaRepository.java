9dda9ca92f3b44d55e77f261165b3af6e38db65b
brayanperutty <brayanperutty@gmail.com>
2024-02-23 21:59:48 -0500

    Ajustes adicionales al commit anterior

--- src/main/java/com/tictac/demo/repository/EvidenciaExperienciaRepository.java
+++ src/main/java/com/tictac/demo/repository/EvidenciaExperienciaRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface EvidenciaExperienciaRepository extends JpaRepository<EvidenciaExperiencia, Integer> {
 
-    List<EvidenciaExperiencia> findByDocenteAutor(String idDocente);
+
 
 }
